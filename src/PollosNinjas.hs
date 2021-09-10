2ae297fc2baac990a4bd2e2c7b35788c0c61f49f
Juan Bono <juanbono94@gmail.com>
2021-09-09 21:15:55 -0300

    subo segunda parte del ejercicio de pollos

--- src/PollosNinjas.hs
+++ src/PollosNinjas.hs
@@ -27,7 +27,13 @@ sinArtesMarciales pollo = null (artesMarciales pollo)
 -- ultimoCampoVacio (UnPollo _ _ _ []) = True
 -- ultimoCampoVacio _ = False
 
--- 4. Pendiente
+-- 4. cruzarPollos (sin usar expresion lambda porque aun no lo vimos)
+cruzar (UnPollo nombre1 diasVivos1 peso1 artes1) (UnPollo nombre2 diasVivos2 peso2 artes2)
+    = UnPollo (nombre1 ++ nombre2) (diasVivos1 + diasVivos2) (peso1 + peso2) (artes1 ++ artes2)
+
+cruzarPollos [unPollo] = unPollo
+cruzarPollos (pollo:(otroPollo:restoDePollos)) 
+    = cruzarPollos ((cruzar pollo otroPollo):restoDePollos)
 
 
 -- Segunda Parte: Pollos Ninjas
@@ -36,4 +42,52 @@ pau = UnPollo "Paula" 2 300 ["judo"]
 
 -- entrenadores
 
+
+--a. arguiniano
+arguiniano pollo = engordar pollo 100
+
+--b miyagi
+agregarSiNoEsta elemento lista
+    | elem elemento lista = lista -- si esta devuelvo la lista
+    | otherwise = elemento:lista -- sino devuelvo una lista con el elemento delante.
+
+miyagi pollo = pollo {artesMarciales = agregarSiNoEsta "karate" (artesMarciales pollo)}
+
+--c marcelito
+marcelito pollo = pollo {artesMarciales = []}
+
+--d brujaTapita
+data Raton = UnRaton { pesoRaton :: Number, -- lo llamo pesoRaton para que no tenga conflicto con el peso del pollo
+                       altura :: Number, 
+                       cantidadBigotes :: Number}
+    deriving Show
+alimentoQueProvee (UnRaton peso altura bigotes) = peso * altura - bigotes
+
+brujaTapita raton pollo = engordar pollo (alimentoQueProvee raton)
+
+--e marioBros, aplicando 2 veces agregarSiNoEsta, para agregar "saltar" y el arte
+-- pasado a la funcion.
+marioBros pollo arteMarcial 
+    = pollo { nombre = "Super mario " ++ (nombre pollo),
+              artesMarciales = agregarSiNoEsta arteMarcial (agregarSiNoEsta "saltar" (artesMarciales pollo)) }
+
+-- Tambien podemos hacer una funcion auxiliar que agregue una lista de elementos
+-- a otra lista si estos no estan presentes.
+agregarVariosSiNoEstan [] lista = lista
+agregarVariosSiNoEstan (e:es) lista = agregarVariosSiNoEstan es (agregarSiNoEsta e lista)
+
+-- marioBros2, usando agregarVariosSiNoEstan
+marioBros2 pollo arteMarcial 
+    = pollo { nombre = "Super mario " ++ (nombre pollo),
+              artesMarciales = agregarVariosSiNoEstan ["saltar", arteMarcial] (artesMarciales pollo)}
+
+-- una tercera opcion, usando @ para poder hacer pattern matching, pero
+-- tener el valor como un todo asignado a una variable, p en este caso.
+-- De esta forma nos ahorramos usar las funciones para acceder cada campo.
+marioBros3 p@(UnPollo nombre _ _ artes) arteMarcial
+    = p {nombre = "Super mario " ++ nombre,
+         artesMarciales = agregarVariosSiNoEstan ["saltar", arteMarcial] artes}
+
+
+-- para los puntos siguientes se requieren temas que no vimos.
 
